72a01a5e9297f60112fa230fdda5671ad7b32317
Pradeep <107491133+pradeep1402@users.noreply.github.com>
2025-06-22 00:53:52 +0530

    pull out into funs

--- src/main/scala/KafkaProducer.scala
+++ src/main/scala/KafkaProducer.scala
@@ -4,15 +4,21 @@ import java.util.Properties
 
 object KafkaProducer {
   def main(args: Array[String]): Unit = {
+    val prop: Properties = config()
+    val producer = new KafkaProducer[String, String](prop)
+    val topic = "trip-tracker"
+
+    val record = new ProducerRecord[String, String](topic, "userId", "Hello Kafka from Scala")
+
+    producer.send(record)
+    producer.close()
+  }
+
+  private def config (): Properties = {
     val prop = new Properties()
     prop.put("bootstrap.servers", "localhost:9092")
     prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-
-    val producer = new KafkaProducer[String, String](prop)
-    val topic = "trip-tracker"
-    val record = new ProducerRecord[String, String](topic, "key", "Hello Kafka from Scala")
-    producer.send(record)
-    producer.close()
+    prop
   }
 }
